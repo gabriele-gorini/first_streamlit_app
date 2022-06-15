7db474a7fdbb64756dd750b83eaacf2638fac91a
gabriele-gorini <101672090+gabriele-gorini@users.noreply.github.com>
2022-06-15 10:41:12 +0200

    Update streamlit_app.py
    Changed the fruit table to show only the selected fruit and not the entire list

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,13 @@ streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 streamlit.header("🍌🥭 Build your own Fruit Smoothie 🥝🍇")
 
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #This is a pick list the user can use to select the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruits_list.loc[fruits_selected]
 
 #Display the list on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
