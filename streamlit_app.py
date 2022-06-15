ec5faf43ed2230070b12b37f7f741d6f1a07abbc
gabriele-gorini <101672090+gabriele-gorini@users.noreply.github.com>
2022-06-15 16:12:16 +0200

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,6 @@ try:
     streamlit.error("Please select a fruit to get information.")
   else:
     streamlit.dataframe(get_fruityvice_data(fruit_choice)
-    
 except URLError as e:
   streamlit.error()
                         
