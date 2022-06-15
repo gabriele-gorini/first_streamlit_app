59f9319a54555e28e94b4469e85423aa9d43042a
gabriele-gorini <101672090+gabriele-gorini@users.noreply.github.com>
2022-06-15 16:01:49 +0200

    Update streamlit_app.py
    Added exception handler for user selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,14 +25,17 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input("What fruit would you like information about?", 'Kiwi')
-streamlit.write("The user entered ", fruit_choice)
-
-fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/" + fruit_choice)
-
-#take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input("What fruit would you like information about?", 'Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+  streamlit.error()
 
 #This line makes the code stop executing so we don't run the insert into snowflake every time we interact with the upper part of the page
 streamlit.stop()
