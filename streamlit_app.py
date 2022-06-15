2b1bfc9af0f96094c3cbfcb86ffbc158a9e8324d
gabriele-gorini <101672090+gabriele-gorini@users.noreply.github.com>
2022-06-15 10:34:01 +0200

    Update streamlit_app.py
    Removed duplicated table display

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,6 @@ streamlit.text("🥑🍞 Avocado Toast")
 streamlit.header("🍌🥭 Build your own Fruit Smoothie 🥝🍇")
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
 
 #This is a pick list the user can use to select the fruit they want to include
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
